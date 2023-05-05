7fd582e347037ba8222f95be8bc21fbfa3fa9695
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2023-05-05 12:28:33 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showString("hola")
+for (let index = 0; index < 5; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+}
+basic.clearScreen()
+basic.showString("adios")
